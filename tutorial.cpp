ffe66b7103b958496008b73d717e178127b913d5
lzb <lzb@huipu2>
2022-08-10 23:43:17 +0800

    cmakedefine unrecognize

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,6 +1,8 @@
-// A simple program that computes the square root of a number
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
 #include <cmath>
-// #include <cstdlib>
 #include <iostream>
 #include <string>
 #include "TutorialConfig.h"
@@ -17,10 +19,13 @@ int main(int argc, char *argv[])
   }
 
   // convert input to double
-  // const double inputValue = atof(argv[1]);
   const double inputValue = std::stod(argv[1]);
-  // calculate square root
+// calculate square root
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
   const double outputValue = sqrt(inputValue);
+#endif
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
